19360b87e1113c0f5ea1d4e32ad80e7ef723ff70
liuliune <164012300+liuliune@users.noreply.github.com>
2024-10-08 16:18:21 +0800

    Add files via upload

---
+++ codec.js
@@ -0,0 +1,12 @@
+function code() {
+        var code1 = Math.floor(Math.random()*100);
+        var code2 = Math.floor(Math.random()*100);
+        window.alert("为了防止黑客攻击，请先完成验证码！")
+        var codei = window.prompt("验证码："+code1+"×"+code2+"的结果？");
+        var codec = code1*code2;
+        if (codei != codec){
+            window.alert("你的验证码不对！请重试！")
+            code()
+        }
+        }
+code()
