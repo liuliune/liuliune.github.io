f3f111d1890bdc167271f936c87ab26643f9b037
liuliune <164012300+liuliune@users.noreply.github.com>
2024-11-26 15:44:41 +0800

    Add files via upload

--- codec.js
+++ codec.js
@@ -1,7 +1,6 @@
 function code() {
         var code1 = Math.floor(Math.random()*100);
         var code2 = Math.floor(Math.random()*100);
-        window.alert("为了防止黑客攻击，请先完成验证码！")
         var codei = window.prompt("验证码："+code1+"×"+code2+"的结果？");
         var codec = code1*code2;
         if (codei != codec){
